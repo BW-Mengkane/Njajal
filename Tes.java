9e99655c9d27016231946f63c3dc3adb226381b2
NafisD2 <nafisdhs2009@gmail.com>
2024-08-03 11:34:49 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,16 @@
+public class Tes {
+    public static void main(String[] args) {
+        String nama = "Nafis";
+        String kelas = "X RPL 5";
+        String alamat = "Banyuwangi";
+        int usia =15;
+        char jenisKelamin ='L';
+        double beratBadan =45.00;
+        System.out.println("Nama: " +nama);
+        System.out.println("Kelas: " +kelas);
+        System.out.println("Asal : " +alamat);
+        System.out.println("Usia: " +usia);
+        System.out.println("jeniskelamin: " +jenisKelamin);
+        System.out.println("beratBadan: " +beratBadan);
+    }
+}
